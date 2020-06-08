ab3bbed29fbdf84cc9459b55989a6cf4a73db284
ParkerZhou <49464811+hiparker@users.noreply.github.com>
2020-06-08 11:17:43 +0800

    Update ApiUtils.java

--- src/main/java/com/parker/api/consumer/util/ApiUtils.java
+++ src/main/java/com/parker/api/consumer/util/ApiUtils.java
@@ -1,19 +1,19 @@
 package com.parker.api.consumer.util;
 
-import com.alibaba.fastjson.JSONArray;
+import lombok.extern.slf4j.Slf4j;
+
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Maps;
 import com.parker.api.common.result.JsonMapper;
 import com.parker.api.common.result.Result;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
@@ -47,32 +47,11 @@ public final class ApiUtils {
     public static Result getToken(String uri,String cnf){
         Result result = null;
         try {
-            Map<String,String> argsMap = Maps.newHashMap();
+            Map<String,Object> argsMap = Maps.newHashMap();
             argsMap.put("cnf",cnf);
-            HttpURLConnection connection = initUrlConn(uri, argsMap);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            String line;
-            /*System.out.println(" ============================= ");
-            System.out.println(" Contents of post request ");
-            System.out.println(" ============================= ");*/
-            StringBuffer stringBuffer = new StringBuffer();
-            while ((line = reader.readLine()) != null) {
-                String sResult = new String(line.getBytes(), "utf-8");
-                stringBuffer.append(sResult);
-                //System.out.println(sResult);
-            }
-
+            String resultStr = sendPost(uri, argsMap);
             // 获得结果
-            result = (Result) JsonMapper.fromJsonString(stringBuffer.toString(), Result.class);
-
-            /*System.out.println(" ============================= ");
-            System.out.println(" Contents of post request ends ");
-            System.out.println(" ============================= ");*/
-            reader.close();
-            connection.disconnect();
-
+            result = (Result) JsonMapper.fromJsonString(resultStr, Result.class);
         }catch (Exception e){
             log.error(e.getMessage(),e);
         }
@@ -90,33 +69,12 @@ public final class ApiUtils {
      * @param argsMap
      * @return
      */
-    public static Result httpRequest(String uri,Map<String,String> argsMap){
+    public static Result httpRequest(String uri,Map<String,Object> argsMap){
         Result result = null;
         try {
-            HttpURLConnection connection = initUrlConn(uri, argsMap);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            String line;
-            /*System.out.println(" ============================= ");
-            System.out.println(" Contents of post request ");
-            System.out.println(" ============================= ");*/
-            StringBuffer stringBuffer = new StringBuffer();
-            while ((line = reader.readLine()) != null) {
-                String sResult = new String(line.getBytes(), "utf-8");
-                stringBuffer.append(sResult);
-                //System.out.println(sResult);
-            }
-
+            String resultStr = sendPost(uri, argsMap);
             // 获得结果
-            result = (Result) JsonMapper.fromJsonString(stringBuffer.toString(), Result.class);
-
-            /*System.out.println(" ============================= ");
-            System.out.println(" Contents of post request ends ");
-            System.out.println(" ============================= ");*/
-            reader.close();
-            connection.disconnect();
-
+            result = (Result) JsonMapper.fromJsonString(resultStr, Result.class);
         }catch (Exception e){
             log.error(e.getMessage(),e);
         }
@@ -127,43 +85,89 @@ public final class ApiUtils {
         return result;
     }
 
-    // HttpURLConnection
-    public static HttpURLConnection initUrlConn(String uri,Map<String,String> argsMap)
-            throws Exception {
-        URL postUrl = new URL(URL.concat(uri));
-        HttpURLConnection connection = (HttpURLConnection) postUrl
-                .openConnection();
-        if (!connection.getDoOutput()) {
-            connection.setDoOutput(true);
+    /** Common */
+    /** ------------------------------------------------------------------------------------------------------------ **/
+
+    /**
+     * 发起 post 请求
+     * @param url
+     * @param argsMap
+     * @return
+     */
+    public static String sendPost(String url, Map<String,Object> argsMap) {
+        DataOutputStream out = null;
+        BufferedReader reader = null;
+        HttpURLConnection conn = null;
+        StringBuffer result = new StringBuffer();
+        try {
+            URL realUrl = new URL(URL.concat(url));
+
+            // 打开和URL之间的连接
+            conn = (HttpURLConnection) realUrl.openConnection();
+            if (!conn.getDoOutput()) {
+                conn.setDoOutput(true);
+            }
+            // 设置为POST请求方式
+            conn.setRequestMethod("POST");
+            conn.setUseCaches(false);
+            conn.setInstanceFollowRedirects(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            // 获取URLConnection对象对应的输出流
+            out = new DataOutputStream(
+                    conn.getOutputStream());
+
+            // 处理请求参数
+            UUID uuid = UUID.randomUUID();
+            int hashCode = uuid.toString().hashCode();
+            argsMap.put("format", 1);
+            argsMap.put("useragent", "ApiClient");
+            argsMap.put("rid", hashCode);
+            argsMap.put("timestamp",  System.currentTimeMillis());
+            argsMap.put("v", "1.0");
+
+            JSONObject jObj = new JSONObject();
+            for (String key : argsMap.keySet()) {
+                jObj.put(key,argsMap.get(key).toString());
+            }
+
+            // 发送请求参数
+            out.writeBytes(chinaToUnicode(jObj.toString()));
+            // flush输出流的缓冲
+            out.flush();
+
+            // 定义BufferedReader输入流来读取URL的响应
+            reader = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String sResult = new String(line.getBytes(), "utf-8");
+                result.append(sResult);
+            }
+
+        } catch (Exception e) {
+            log.error("发送 POST 请求出现异常！ Message:{}",e.getMessage(),e);
         }
-        connection.setRequestMethod("POST");
-        connection.setUseCaches(false);
-        connection.setInstanceFollowRedirects(true);
-        connection.setRequestProperty("Content-Type", "application/json");
-        DataOutputStream out = new DataOutputStream(
-                connection.getOutputStream());
-
-        UUID uuid = UUID.randomUUID();
-        int hashCode = uuid.toString().hashCode();
-
-
-        JSONObject jObj = new JSONObject();
-        jObj.put("format", 1);
-        jObj.put("useragent", "ApiClient");
-        jObj.put("rid", hashCode);
-        jObj.put("timestamp",  System.currentTimeMillis());
-        jObj.put("v", "1.0");
-        for (String key : argsMap.keySet()) {
-            jObj.put(key,chinaToUnicode(argsMap.get(key)));
+        //使用finally块来关闭输出流、输入流、链接
+        finally{
+            try{
+                if(out!=null){
+                    out.close();
+                }
+                if(reader!=null){
+                    reader.close();
+                }
+                if(conn != null){
+                    conn.disconnect();
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
         }
-
-        out.writeBytes(jObj.toString());
-        out.flush();
-        out.close();
-
-        return connection;
+        return result.toString();
     }
-
 
     /**
      * 把中文转成Unicode码
